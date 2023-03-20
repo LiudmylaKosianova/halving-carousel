607642d1faf92be9c81ccb1fc6bb36cba3aab649
Liudmyla Kosianova <snw.wht.lk@gmail.com>
2023-03-20 19:00:42 +0000

    Update CarouselRun.java

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,48 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    protected int currentPosition = 0;
+    protected int [] containRunning;
+
+    public CarouselRun(int[]greenSofa) {
+        this.containRunning = greenSofa;
+    }
+
+    public boolean onlyZeros (){
+        for(int element: containRunning){
+            if(element!=0){
+               return false;}
+        }
+
+        return true;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+         if(onlyZeros()){
+            return -1;
+        }
+
+
+        while(containRunning[currentPosition]==0){
+            currentPosition++;
+            if(currentPosition==containRunning.length){
+                currentPosition=0;
+            }
+        }
+
+        int willReturn = containRunning[currentPosition];
+        containRunning[currentPosition] = containRunning[currentPosition]/2;
+        currentPosition++;
+
+        if(currentPosition==containRunning.length){
+            currentPosition=0;
+        }
+        return willReturn;
+       
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        return onlyZeros();    
     }
 
 }
