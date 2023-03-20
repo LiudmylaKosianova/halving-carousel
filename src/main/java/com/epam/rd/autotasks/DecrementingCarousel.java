7dfcff1c87d9bd5b40e4f2751b5166da39f79af6
Liudmyla Kosianova <snw.wht.lk@gmail.com>
2023-03-20 19:05:06 +0000

    Update DecrementingCarousel.java

--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,55 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
 
+    private int capacity;
+    protected int[] contain;
+
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        this.contain = new int[capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        
+         int number = contain.length;
+
+        if (element > 0 && capacity > 0 && !isAlreadyRun) { 
+            contain[number-capacity] = element;
+            capacity--;
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public CarouselRun run(){
-       throw new UnsupportedOperationException();
+     
+    protected boolean isAlreadyRun = false;
+    
+    public CarouselRun run() {
+        
+        if (!isAlreadyRun) {
+            isAlreadyRun = true;
+            return new CarouselRun(contain);
+        } else {
+            return null;
+        }        
+    }
+
+     public int getCapacity() {
+        return capacity;
+    }
+
+    public int[] getContain() {
+        return contain;
+    }
+
+    public int getElement(int in){
+        return contain [in];
+    }
+
+    public int getLength(){
+        return contain.length;
     }
 }
